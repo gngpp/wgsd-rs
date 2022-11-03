a41275d4edc4f6c4bf24ac9ae9f6206c0d8a9cbb
relife <verticle@foxmail.com>
2022-11-03 12:11:57 +0800

    update

--- src/conf/model.rs
+++ src/conf/model.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use std::ops::Deref;
 
 extern crate ipnet;
@@ -14,33 +15,52 @@ impl WgConfig {
             peer_list,
         }
     }
-    pub fn print(&self) {
+    pub fn get_config(&self) -> Vec<String> {
         let mut lines: Vec<String> = Vec::new();
         lines.push(self.interface.tag());
         lines.push(String::from("[Interface]"));
         lines.push(format!("Address = {}", self.interface.address()));
-        lines.push(format!("PrivateKey = {}", ""));
-        lines.push(format!("ListenPort = {}", ""));
+        lines.push(format!("PrivateKey = {}", self.interface.private_key()));
+        lines.push(format!("ListenPort = {}", self.interface.listen_port()));
+        lines.push(format!("MTU = {}\n", self.interface.mtu()));
+
+        for peer in self.peer_list.iter() {
+            lines.push(peer.tag());
+            lines.push(String::from("[Peer]"));
+            lines.push(format!("PublicKey = {}", peer.public_key()));
+            lines.push(format!("AllowedIPs = {}", peer.allowed_ips()));
+            lines.push(format!("Endpoint = {}", peer.endpoint()));
+            lines.push(format!(
+                "PersistentKeepalive = {}\n",
+                peer.persistent_keepalive()
+            ));
+        }
+        lines
     }
 }
 
 pub struct Interface {
+    // node mode (client/server)
+    is_server: bool,
+    // tag name
     tag: Option<String>,
-    // address
+    // interface WireGuard address
     address: Option<Vec<ipnet::IpNet>>,
-    // server's privatekey
+    // interface private key
     private_key: Option<String>,
-    // server's listent port
+    // interface listen port
     listen_port: Option<String>,
-
+    // interface MTU
     mtu: Option<u32>,
-    preup: Option<String>,
-    postup: Option<String>,
-    predown: Option<String>,
-    postdown: Option<String>,
+
+    pre_up: Option<String>,
+    post_up: Option<String>,
+    pre_down: Option<String>,
+    post_down: Option<String>,
 }
 
 impl Interface {
+    //noinspection DuplicatedCode
     pub fn tag(&self) -> String {
         if let Some(ref tag) = self.tag {
             return format!("#{}", tag.to_string());
@@ -49,80 +69,159 @@ impl Interface {
     }
 
     pub fn address(&self) -> String {
-        if let Some(ref ipnet_address) = self.address {
-            
+        if let Some(ref address_list) = self.address {
+            return address_list
+                .iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<String>>()
+                .join(",");
         }
-        String::new()
+        return String::from("empty");
+    }
+
+    pub fn private_key(&self) -> String {
+        if let Some(ref private_key) = self.private_key {
+            return private_key.to_string();
+        }
+        return String::from("empty");
+    }
+
+    pub fn listen_port(&self) -> String {
+        if let Some(ref listen_port) = self.listen_port {
+            return listen_port.to_string();
+        }
+        return String::from("empty");
+    }
+
+    pub fn mtu(&self) -> String {
+        if let Some(ref mtu) = self.mtu {
+            return mtu.to_string();
+        }
+        return String::from("empty");
     }
 }
 
 pub struct Peer {
-    pub tag: Option<String>,
-    // server's public key
-    pub public_key: Option<String>,
-    // server's router allowed_ips
-    pub allowed_ips: Option<Vec<ipnet::IpNet>>,
+    // tag name
+    tag: Option<String>,
+    // peer's public key
+    public_key: Option<String>,
+    // peer's router allowed_ips
+    allowed_ips: Option<Vec<ipnet::IpNet>>,
     // keep alive interval
-    pub persistent_keepalive: Option<u16>,
-    // peet endpiont
-    pub endpoint: Option<PeerEndpoint>,
+    persistent_keepalive: Option<u16>,
+    // peet endpoint
+    endpoint: Option<PeerEndpoint>,
+}
+
+impl Peer {
+    //noinspection DuplicatedCode
+    pub fn tag(&self) -> String {
+        if let Some(ref tag) = self.tag {
+            return format!("#{}", tag.to_string());
+        }
+        return String::new();
+    }
+
+    pub fn allowed_ips(&self) -> String {
+        if let Some(ref allowed_ips) = self.allowed_ips {
+            return allowed_ips
+                .iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<String>>()
+                .join(",");
+        }
+        return String::from("empty");
+    }
+
+    pub fn public_key(&self) -> String {
+        if let Some(ref public_key) = self.public_key {
+            return public_key.to_string();
+        }
+        return String::from("empty");
+    }
+
+    pub fn persistent_keepalive(&self) -> String {
+        if let Some(ref persistent_keepalive) = self.persistent_keepalive {
+            return persistent_keepalive.to_string();
+        }
+        return String::from("empty");
+    }
+
+    pub fn endpoint(&self) -> String {
+        if let Some(ref endpoint) = self.endpoint {
+            return endpoint.to_string();
+        }
+        return String::from("empty");
+    }
 }
 
 pub struct Node {
-    pub tag: Option<String>,
-    // address
-    pub address: Option<Vec<ipnet::IpNet>>,
-    // server's public key
-    pub public_key: Option<String>,
-    // server's privatekey
-    pub private_key: Option<String>,
-    // server's listent port
-    pub listen_port: Option<String>,
-    // server's router allowed_ips
-    pub allowed_ips: Option<Vec<ipnet::IpNet>>,
+    // node mode (client/server)
+    is_server: bool,
+    // peer tag name
+    tag: Option<String>,
+    // server peer address
+    address: Option<Vec<ipnet::IpNet>>,
+    // peet's public key
+    public_key: Option<String>,
+    // peet's private key
+    private_key: Option<String>,
+    // peet's listen port
+    listen_port: Option<String>,
+    // peet's router allowed ips
+    allowed_ips: Option<Vec<ipnet::IpNet>>,
+    // peet's endpoint router allowed ips
+    endpoint_allowed_ips: Option<Vec<ipnet::IpNet>>,
     // keep alive interval
-    pub persistent_keepalive: Option<u16>,
-    // peet endpiont
-    pub endpoint: Option<PeerEndpoint>,
+    persistent_keepalive: Option<u16>,
+    // peet endpoint
+    endpoint: Option<PeerEndpoint>,
 
     pub mtu: Option<u32>,
 
-    pub preup: Option<String>,
+    pub pre_up: Option<String>,
 
-    pub postup: Option<String>,
+    pub post_up: Option<String>,
 
-    pub predown: Option<String>,
+    pub pre_down: Option<String>,
 
-    pub postdown: Option<String>,
+    pub post_down: Option<String>,
 }
 
 impl Node {
-    pub fn to_interface(self) -> Interface {
-        Interface {
-            tag: self.tag,
-            address: self.address,
-            private_key: self.private_key,
-            listen_port: self.listen_port,
-            mtu: self.mtu,
-            preup: self.preup,
-            postdown: self.postdown,
-            predown: self.predown,
-            postup: self.postup,
-        }
+    pub fn to_interface(self) -> (Interface, Option<String>) {
+        (
+            Interface {
+                is_server: self.is_server,
+                tag: self.tag,
+                address: self.address,
+                private_key: self.private_key,
+                listen_port: self.listen_port,
+                mtu: self.mtu,
+                pre_up: self.pre_up,
+                post_down: self.post_down,
+                pre_down: self.pre_down,
+                post_up: self.post_up,
+            },
+            self.public_key,
+        )
     }
 
-    pub fn to_peer(self) -> Peer {
-        Peer {
-            tag: self.tag,
-            public_key: self.public_key,
-            allowed_ips: self.allowed_ips,
-            persistent_keepalive: self.persistent_keepalive,
-            endpoint: self.endpoint,
-        }
+    pub fn to_peer(self) -> (Peer, Option<String>) {
+        (
+            Peer {
+                tag: self.tag,
+                public_key: self.public_key,
+                allowed_ips: self.allowed_ips,
+                persistent_keepalive: self.persistent_keepalive,
+                endpoint: self.endpoint,
+            },
+            self.private_key,
+        )
     }
 }
 
-#[derive(Debug)]
 pub struct PeerEndpoint {
     address: std::net::IpAddr,
     port: u16,
@@ -132,7 +231,7 @@ impl PeerEndpoint {
     pub fn new(addr: std::net::IpAddr, port: u16) -> Self {
         Self {
             address: addr,
-            port: port,
+            port,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use clap::{Arg, ArgAction, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command};
+use std::io::{Read, Write};
 pub(crate) mod conf;
 
 fn main() -> anyhow::Result<()> {
-    let matches = Command::new("wgsd-rs")
+    let matches = Command::new("wgsdc-rs")
+        .name("wgsdc")
         .version("1.0")
+        .propagate_version(true)
+        .subcommand_required(false)
+        .arg_required_else_help(true)
         .author("gngpp. <gngppz@gmail.com>")
         .about("WireGuard configure the service discovery tool")
+        .subcommand(
+            Command::new("add-peer")
+                .about("Add a new peer")
+                .propagate_version(true)
+                .subcommand_required(false)
+                .arg_required_else_help(true)
+                .arg(
+                    Arg::new("addr")
+                        .long("addr")
+                        .help("Required configuration file to use"),
+                ),
+        )
         .arg(
             Arg::new("server")
                 .short('s')
@@ -19,46 +36,70 @@ fn main() -> anyhow::Result<()> {
                 .short('c')
                 .long("client")
                 .value_name("host")
+                .action(ArgAction::Set)
                 .value_parser(conf::util::verify_host)
                 .group("mode")
+                .requires("port")
                 .help("Run in client mode, connecting to <host>"),
         )
         .arg(
             Arg::new("port")
                 .short('p')
                 .long("port")
+                .action(ArgAction::Set)
                 .value_parser(conf::util::verify_port_in_range)
                 .help("Bind to a specific client/server port (TCP, temporary port by default)")
-                .required(true),
+                .requires("client")
         )
-        .arg_required_else_help(true)
+        .arg(
+            Arg::new("token")
+                .long("token")
+                .help("Connect server token")
+                .requires("client"),
+        )
         .get_matches();
 
-    // Note, it's safe to call unwrap() because the arg is required
-    let port = *matches
-        .get_one::<u16>("port")
-        .expect("'port' is required and parsing will fail if its missing");
+    client_handler(&matches)?;
+    server_handler(&matches)?;
+    Ok(())
+}
 
-    match matches.get_one::<std::net::IpAddr>("client") {
-        Some(addr) => {
-            if addr.is_ipv6() {
-                println!("{} is ipv6", addr.to_string());
-            }
-            if addr.is_ipv4() {
-                println!("{} is ipv4", addr.to_string());
-            }
-        }
-        None => {
-            let v4: std::net::Ipv4Addr = "0.0.0.0".parse::<std::net::Ipv4Addr>().unwrap();
-            let v6: std::net::Ipv6Addr = "::".parse::<std::net::Ipv6Addr>().unwrap();
-            let addrs = [
-                std::net::SocketAddr::from((std::net::IpAddr::V4(v4), port)),
-                std::net::SocketAddr::from((std::net::IpAddr::V6(v6), port)),
-            ];
-            let tcp = std::net::TcpListener::bind(&addrs[..])?;
-            println!("server listent to {}", v4.to_string());
-            for _t in tcp.incoming() {}
-        }
+fn client_handler(arg: &ArgMatches) -> anyhow::Result<()> {
+    if let Some(addr) = arg.get_one::<std::net::IpAddr>("client") {
+        let port = *arg.get_one::<u16>("port").unwrap_or(&(0 as u16));
+        let socket = std::net::SocketAddr::new(*addr, port);
+        let mut connect = std::net::TcpStream::connect(socket)?;
+        println!("connect to {}", connect.local_addr()?);
+
+        connect.write(b"client")?;
     };
+    Ok(())
+}
+
+fn server_handler(arg: &ArgMatches) -> anyhow::Result<()> {
+    match arg.get_one::<bool>("server") {
+        None => {}
+        Some(b) => {
+            if *b {
+                let port = *arg.get_one::<u16>("port").unwrap_or(&(0 as u16));
+                let v4: std::net::Ipv4Addr = "0.0.0.0".parse::<std::net::Ipv4Addr>()?;
+                let v6: std::net::Ipv6Addr = "::".parse::<std::net::Ipv6Addr>()?;
+                let addrs = [
+                    std::net::SocketAddr::from((std::net::IpAddr::V4(v4), port)),
+                    std::net::SocketAddr::from((std::net::IpAddr::V6(v6), port)),
+                ];
+                let tcp_listen = std::net::TcpListener::bind(&addrs[..])?;
+                println!("server listen to {}", tcp_listen.local_addr()?);
+                loop {
+                    for incoming in tcp_listen.accept() {
+                        let mut tcp_stream = incoming.0;
+                        let mut input = String::new();
+                        let _ = tcp_stream.read_to_string(&mut input)?;
+                        println!("{:?} says {}", incoming.1, input);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
