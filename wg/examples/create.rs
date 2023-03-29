66a4007613f0e8e74f86c0b6467d74fd343fafc3
gngpp <gngppz@gmail.com>
2023-03-29 16:19:58 +0800

    refactor: update examples

--- wg/examples/create.rs
+++ wg/examples/create.rs
@@ -5,6 +5,8 @@ use wg::{AllowedIp, Backend, Device, DeviceUpdate, KeyPair, PeerConfigBuilder};
 const BACKEND: Backend = Backend::Kernel;
 #[cfg(not(target_os = "linux"))]
 const BACKEND: Backend = Backend::Userspace;
+#[cfg(not(target_os = "macos"))]
+const BACKEND: Backend = Backend::Userspace;
 
 fn main() {
     if unsafe { libc::getuid() } != 0 {
@@ -18,21 +20,21 @@ fn main() {
 
     // add peer
     let keypair_list: Vec<_> = (0..2).map(|_| KeyPair::generate()).collect();
-    for x in keypair_list.iter().enumerate() {
-        let ipv4addr = Ipv4Addr::new(100, 100, (100 + x.0) as u8, 10);
-        let peer_config_builder = PeerConfigBuilder::new(&x.1.public)
-            .add_allowed_ip(IpAddr::V4(Ipv4Addr::new(192, 168, 10, (2 + x.0) as u8)), 32)
+    for keypair in keypair_list.iter().enumerate() {
+        let ipv4addr = Ipv4Addr::new(100, 100, (100 + keypair.0) as u8, 10);
+        let peer_config_builder = PeerConfigBuilder::new(&keypair.1.public)
+            .add_allowed_ip(IpAddr::V4(Ipv4Addr::new(192, 168, 10, (2 + keypair.0) as u8)), 32)
             .add_allowed_ips(&[AllowedIp::new(IpAddr::V4(ipv4addr), 24)]);
         builder = builder.add_peer(peer_config_builder)
     }
 
-    // interface alias
-    let interface = "test".parse().unwrap();
-    builder.apply(&interface, BACKEND).unwrap();
+    // interface alias name
+    let interface_name = "test".parse().unwrap();
+    builder.apply(&interface_name, BACKEND).unwrap();
 
-    println!("create wireguard interfaces: {}", interface.to_string());
+    println!("create wireguard interfaces: {}", interface_name.to_string());
 
-    let device = Device::get(&interface, Backend::Userspace).unwrap();
+    let device = Device::get(&interface_name, Backend::Userspace).unwrap();
 
     for keypair in &keypair_list {
         assert!(device
--- wg/examples/quickup.rs
+++ wg/examples/quickup.rs
@@ -15,13 +15,26 @@ fn main() {
     let name = "test";
 
     let pair = KeyPair::generate();
-    let mut builder = wg::tools::quick::WgQuick::new(name).unwrap();
-    builder = builder
+    let mut wgquick_builder = wg::tools::quick::WgQuick::new(name).unwrap();
+    wgquick_builder = wgquick_builder
         .set_keypair(pair)
         .set_address("192.168.10.1/24".parse().unwrap())
         .set_listen_port(51822);
 
-    builder.apply(BACKEND).unwrap();
+    // add peer
+    let keypair_list: Vec<_> = (0..2).map(|_| KeyPair::generate()).collect();
+    for keypair in keypair_list.iter().enumerate() {
+        let ipv4addr = Ipv4Addr::new(100, 100, (100 + keypair.0) as u8, 10);
+        let peer_config_builder = PeerConfigBuilder::new(&keypair.1.public)
+            .add_allowed_ip(
+                IpAddr::V4(Ipv4Addr::new(192, 168, 10, (2 + keypair.0) as u8)),
+                32,
+            )
+            .add_allowed_ips(&[AllowedIp::new(IpAddr::V4(ipv4addr), 24)]);
+        wgquick_builder = wgquick_builder.add_peer(peer_config_builder)
+    }
+
+    wgquick_builder.apply(BACKEND).unwrap();
 
     println!("create wireguard interfaces: {}", name);
 
